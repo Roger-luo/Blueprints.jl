16b4abfb435a165e18c6215c2c769e4dc49f797e
Roger-Luo <rogerluo.rl18@gmail.com>
2022-02-08 13:30:57 -0500

    wrap up

--- src/blueprints/git.jl
+++ src/blueprints/git.jl
@@ -1,5 +1,35 @@
-@option struct Git
+@option "github" struct GitHubRepo
+    type::Reflect
+    user::String # user may not be the same as user.name
+    repo::Maybe{String} # if nothing use pkg name
 end
 
+# @option "gitlab" struct GitLabRepo
+#     type::Reflect
+#     user::String
+#     repo::String
+# end
+
+@option struct Git
+    name::Maybe{String}
+    email::Maybe{String}
+    branch::Maybe{String}
+    ssh::Bool = false
+    jl::Bool = true
+    manifest::Bool = false
+    gpgsign::Bool = false
+    repo::Maybe{GitHubRepo}
+end
+
+# setup git repo
+function preprocess(bp::Git, ctx::Context)
+end
+
+# create .gitignore
 function compile(bp::Git, ctx::Context)
+    ignore_list = get(ctx.state, :gitignore, String[])
+end
+
+# commit changes
+function postprocess(bp::Git, ctx::Context)
 end
