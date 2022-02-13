926e262e75db658d51c5472a8d28a7b51d8b4ecc
Roger-Luo <rogerluo.rl18@gmail.com>
2022-02-13 02:12:38 -0500

    add more docstrings

--- src/types.jl
+++ src/types.jl
@@ -1,12 +1,50 @@
+"""
+    @option struct Blueprint
+
+The general type `Blueprint` contains a list of other blueprints.
+
+# Fields
+
+- `name::String`: the name of this blueprint
+- `config_file::Maybe{String}`: the corresponding config file name of this blueprint.
+- `meta:Dict{String, Any}`: meta info from config file or other places.
+- `blueprints::Vector{Any}`: list of blueprint objects to compile.
+"""
 @option struct Blueprint
     name::String # name of this blueprint
     config_file::Maybe{String} # can be nothing if no config file
     meta::Dict{String, Any} = Dict{String, Any}()
     blueprints::Vector{Any} = []
+
+    function Blueprint(
+        name::String, config_file::Maybe{String},
+        meta::Dict{String, Any}, blueprints::Vector{Any}
+    )
+        new(name, config_file, meta, sort(blueprints, by=priority))
+    end
 end
 
 Base.show(io::IO, ::MIME"text/plain", x::Blueprint) = GarishPrint.pprint_struct(io, x)
 
+"""
+    priority(blueprint)
+
+Return the priority of `blueprint`. Default is `1000`.
+"""
+priority(blueprint) = 1000 # defaul priority
+
+"""
+    @option struct Context
+
+Type for storing the compile context.
+    
+# Fields
+
+- `args::Tuple`: the input arguments.
+- `kwargs::NamedTuple`: the input keyword arguments.
+- `stack::NTuple{N, Blueprint}`: [`Blueprint`](@ref) stack, topmost is the current blueprint under compile.
+- `state::Dict{Symbol, Any}`: compile states that each blueprint can pass in.
+"""
 @option struct Context{N, Args <: Tuple, Kwargs <: NamedTuple}
     # global inputs that is
     # not from config meta
